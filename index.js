4ac4a1123c0d10c20d8c0838ef09e16f980b54e5
Elvis <elvisgarcia93@gmail.com>
2022-09-03 15:55:07 -0400

    Include Conditional Statement for values beyond the current value

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function selectionSort(arr){
     // First Loop to Iterate through the Array
     for(let i = 0; i < arr.length; i++){
+        // Setting minimum value to current index
         let min = i;
+        // Include the Inner Loop
         for(let j = i + 1; j < arr.length; j++){
-            
+            // Check if any value is less than the value at i
+            if(arr[i] > arr[j]){
+                
+            }
         }
     }
 }
