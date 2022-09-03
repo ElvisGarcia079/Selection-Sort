79abac52313554250dd6f39a2beea6b6643b6304
Elvis <elvisgarcia93@gmail.com>
2022-09-03 15:53:46 -0400

    Add Inner For Loop

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ function selectionSort(arr){
     // First Loop to Iterate through the Array
     for(let i = 0; i < arr.length; i++){
         let min = i;
+        for(let j = i + 1; j < arr.length; j++){
+            
+        }
     }
 }
